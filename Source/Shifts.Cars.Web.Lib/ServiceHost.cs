5534d3cb3a6b6c7f5eace5f62132447fc1c764a5
Anders Juul <anders_privat@hotmail.com>
2016-12-25 19:06:47 +0100

    a

--- Source/Shifts.Cars.Web.Lib/ServiceHost.cs
+++ Source/Shifts.Cars.Web.Lib/ServiceHost.cs
@@ -1,17 +1,14 @@
 ﻿using System;
+using System.Configuration;
 using Microsoft.Owin.Hosting;
 
 namespace Shifts.Cars.Web.Lib
 {
     public class ServiceHost
     {
-        // Create the variable for our self-hosted server
-        //
         private IDisposable _server;
 
-        // Just hard-code the address for now
-        //
-        private string _baseAddress = "http://localhost:7331";
+        private string _baseAddress ;
 
         public ServiceHost()
         {
@@ -22,10 +19,7 @@ namespace Shifts.Cars.Web.Lib
         {
             Console.WriteLine("ServiceHost started");
 
-            // Start up the server by providing our OWIN Startup class as the source type.
-            //  We also save the return object so we can dispose of it properly when the
-            //  service is shutdown
-            //
+            _baseAddress = ConfigurationManager.AppSettings["shifts.cars.api.url"];    
             _server = WebApp.Start<Startup>(url: _baseAddress);
 
             Console.WriteLine($"Server running at {_baseAddress}");
@@ -41,8 +35,6 @@ namespace Shifts.Cars.Web.Lib
         {
             Console.WriteLine("Server shutting down");
 
-            // Dispose of the server object since we're shutting everything down
-            //
             _server.Dispose();
 
             Console.WriteLine("ServiceHost stopped");

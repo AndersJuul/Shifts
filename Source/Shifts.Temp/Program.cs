126edce9d7d2501a9f4647bb05b0dd0508f4c77b
Anders Juul <anders_privat@hotmail.com>
2016-12-26 13:59:47 +0100

    a

---
+++ Source/Shifts.Temp/Program.cs
@@ -0,0 +1,39 @@
+﻿using Topshelf;
+
+namespace Shifts.Temp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<MyService>(config =>
+                {
+                    config.ConstructUsing(o => new MyService());
+                    config.WhenStarted(o => o.Start());
+                    config.WhenStopped(o => { o.Stop();  });
+                });
+
+                x.RunAsLocalSystem();
+                x.SetServiceName("ServiceName");
+                x.SetDescription("ServiceDesc");
+                x.SetDisplayName("ServiceDispName");
+
+            });
+        }
+    }
+
+    internal class MyService
+    {
+        public void Stop()
+        {
+            
+        }
+
+        public void Start()
+        {
+            
+        }
+    }
+}

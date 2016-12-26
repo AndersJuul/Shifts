922c6aba81a30962b5a32f962eeae566ee7fa644
Anders Juul <anders_privat@hotmail.com>
2016-12-26 13:13:52 +0100

    a

--- Source/Shifts.Cars.Web.Lib/ServiceHost.cs
+++ Source/Shifts.Cars.Web.Lib/ServiceHost.cs
@@ -15,8 +15,8 @@ namespace Shifts.Cars.Web.Lib
         {
             //Log.Logger.Information("ServiceHost starting");
 
-            _baseAddress = ConfigurationManager.AppSettings["shifts.cars.api.url"];    
-            _server = WebApp.Start<Startup>(url: _baseAddress);
+            //_baseAddress = ConfigurationManager.AppSettings["shifts.cars.api.url"];    
+            //_server = WebApp.Start<Startup>(url: _baseAddress);
 
             //Log.Logger.Information($"Server running at {_baseAddress}");
         }
@@ -30,7 +30,7 @@ namespace Shifts.Cars.Web.Lib
         {
             //Log.Logger.Information("Server shutting down");
 
-            _server.Dispose();
+            //_server.Dispose();
 
             //Log.Logger.Information("ServiceHost stopped");
         }

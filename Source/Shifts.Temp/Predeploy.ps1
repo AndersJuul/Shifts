5e81592ada6cac436716a8125cacbde9fd518f07
Anders Juul <anders_privat@hotmail.com>
2016-12-28 08:29:22 +0100

    a

---
+++ Source/Shifts.Temp/Predeploy.ps1
@@ -0,0 +1,21 @@
+﻿function Get-ServiceExePath ($name)
+{
+    $service = Get-WmiObject win32_service | ?{$_.Name -eq $name} 
+    $path = $service | select @{Name="Path"; Expression={$_.PathName.split('"')[1]}} 
+    $path.Path
+}
+
+$ServiceName = $OctopusParameters['MyServiceName']
+$ExePath = Get-ServiceExePath $ServiceName
+
+Write-Host "Removing service: " + $ServiceName
+if ($ExePath)
+{
+    Write-Host "Service executable: " + $ExePath
+    & $ExePath uninstall
+    Write-Host "Service removed."
+}
+else
+{
+    Write-Host "Service not found: " + $ServiceName
+}

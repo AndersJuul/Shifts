650599db358d06fb3aed01a82a0186b2fd46d7ec
Anders Juul <anders_privat@hotmail.com>
2016-12-29 18:42:32 +0100

    a

--- Source/Shifts.Temp/Deploy.ps1
+++ Source/Shifts.Temp/Deploy.ps1
@@ -1,13 +1,13 @@
-﻿Write-Host "Parameter: " + #{Octopus.Environment.Name}
+﻿Write-Host "Parameter: #{Octopus.Environment.Name}"
+Write-Host "ExePath : #{Octopus.Action[Shifts.Temp].WindowsService.ExecutablePath}"
 
-$ServiceName = "#{Octopus.Environment.Name}" + "-Shifts.Temp"
-
-Write-Host "Removing service: " $ServiceName
+$ServiceName = "#{Octopus.Environment.Name}-Shifts.Temp"
+Write-Host "Service Name: $ServiceName"
 
 if ($ExePath)
 {
     Write-Host "Service executable: Shifts.Temp.exe"
-    & Shifts.Temp.exe uninstall
+    & #{Octopus.Action[Shifts.Temp].WindowsService.ExecutablePath} uninstall
     Write-Host "Service removed."
 }
 else
@@ -16,6 +16,6 @@ else
 }
 
 write-host "Installing service: " $ServiceName
-& Shifts.Temp.exe install --autostart -servicename $ServiceName
+& #{Octopus.Action[Shifts.Temp].WindowsService.ExecutablePath} install --autostart -servicename $ServiceName
 Start-Service $ServiceName
 write-host "Service installed: " + $ServiceName

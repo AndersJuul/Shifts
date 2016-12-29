eb1254be38fc2ed37de3f51ab86ad9d25a49e09e
Anders Juul <anders_privat@hotmail.com>
2016-12-29 16:36:42 +0100

    a

--- Source/Shifts.Temp/Deploy.ps1
+++ Source/Shifts.Temp/Deploy.ps1
@@ -2,24 +2,14 @@
     [Parameter(Mandatory=$True)]
     [string]$Environment,
     [Parameter(Mandatory=$True)]
-    [string]$InstDir,
-    [Parameter(Mandatory=$True)]
     [string]$InstPath
 )
 
-function Get-ServiceExePath ($name)
-{
-    $service = Get-WmiObject win32_service | ?{$_.Name -eq $name} 
-    $path = $service | select @{Name="Path"; Expression={$_.PathName.split('"')[1]}} 
-    $path.Path
-}
-
 Write-Host "Parameter: " + $Environment
-Write-Host "Parameter: " + $InstDir
 Write-Host "Parameter: " + $InstPath
 
-$ServiceName = "Shifts.Temp"
-$ExePath = Get-ServiceExePath $ServiceName
+$ServiceName = $Environment"Shifts.Temp"
+$ExePath = "Shifts.Temp.exe"
 
 Write-Host "Removing service: " $ServiceName
 if ($ExePath)

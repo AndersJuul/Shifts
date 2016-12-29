a024865011bc949c71a9d3e34af97f056f753476
Anders Juul <anders_privat@hotmail.com>
2016-12-29 08:20:19 +0100

    a

--- Source/Shifts.Temp/Deploy.ps1
+++ Source/Shifts.Temp/Deploy.ps1
@@ -18,28 +18,3 @@ Write-Host "Parameter: " + $Environment
 Write-Host "Parameter: " + $InstDir
 Write-Host "Parameter: " + $InstPath
 
-$ServiceName = $OctopusParameters['MyServiceName']
-$ExePath = Get-ServiceExePath $ServiceName
-
-Write-Host "Removing service: " + $ServiceName
-if ($ExePath)
-{
-    Write-Host "Service executable: " + $ExePath
-    $ExePath uninstall
-    Write-Host "Service removed."
-}
-else
-{
-    Write-Host "Service not found: " + $ServiceName
-}
-
-
-
-
-$InstPath
-
-write-host "Installing service: " + $ServiceName
-write-host "Executable: " + $InstPath
-$InstPath install --autostart
-Start-Service $ServiceName
-write-host "Service installed: " + $ServiceName

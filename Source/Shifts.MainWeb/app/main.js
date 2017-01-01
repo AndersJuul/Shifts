f433ff9384aef7c83e4921fded9f13dc7a56fa9d
Anders Juul <anders_privat@hotmail.com>
2017-01-01 19:06:00 +0100

    a

---
+++ Source/Shifts.MainWeb/app/main.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('Main', main);
+
+    function main() {
+        var vm = this;
+        vm.food = 'pizza';
+    }
+
+})();

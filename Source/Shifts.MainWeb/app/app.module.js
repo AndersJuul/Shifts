1a28e329bff239f494e689d1c92b1791d2de0a03
Anders Juul <anders_privat@hotmail.com>
2017-01-02 19:52:54 +0100

    a

--- Source/Shifts.MainWeb/app/app.module.js
+++ Source/Shifts.MainWeb/app/app.module.js
@@ -1,6 +1,15 @@
 ﻿(function () {
     'use strict';
 
-    angular.module('app', []);
-
+    var app = angular.module('app', ["ngRoute"]);
+    app.config(function ($routeProvider) {
+        $routeProvider
+            .when("/main", {
+                templateUrl: "main.html",
+                controller: "MainController"
+            })
+            .otherwise(
+            { redirectTo: "/main" }
+            );
+    });
 })();

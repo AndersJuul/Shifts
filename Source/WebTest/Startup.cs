563a9fc5814cef62ae60429f77b42ff0b805916f
Anders Juul <anders_privat@hotmail.com>
2017-01-15 15:03:00 +0100

    a

---
+++ Source/WebTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebTest.Startup))]
+namespace WebTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

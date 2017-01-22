0626940813f767b30fc8125225ab87cba0e14695
Anders Juul <anders_privat@hotmail.com>
2017-01-22 22:19:04 +0100

    a

---
+++ Source/Ajf.Selenium/LoginPage.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium.Chrome;
+
+namespace Ajf.Selenium
+{
+    public class LoginPage
+    {
+        public static void Go()
+        {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("test-type");
+            var driver = new ChromeDriver(@"d:\selenium\", chromeOptions);
+
+            driver.Navigate().GoToUrl("http://www.google.com");
+        }
+    }
+}
---
+++ Source/Shifts.SeleniumTests/NUnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Ajf.Selenium;
+using NUnit.Framework;
+
+//using NUnit.Framework;
+
+namespace Shifts.SeleniumTests
+{
+    [TestFixture]
+    public class NUnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            LoginPage.Go();
+        }
+    }
+}

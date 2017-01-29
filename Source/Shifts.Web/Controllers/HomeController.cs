e01de610105bacf0bc990852a940cbbf74002009
Anders Juul <anders_privat@hotmail.com>
2017-01-29 22:03:53 +0100

    a

--- Source/Shifts.Web/Controllers/HomeController.cs
+++ Source/Shifts.Web/Controllers/HomeController.cs
@@ -7,8 +7,6 @@ namespace WebApplication1.Controllers
         public ActionResult Index()
         {
             return View();
-            //return File(Server.MapPath("Static/") + "index.html", "text/html");
-            //return File("index.html", "text/html");
         }
     }
 }

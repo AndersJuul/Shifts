4948fe22365a4205dc9e2747f2c7ad1bbc51dd00
Anders Juul <anders_privat@hotmail.com>
2017-01-31 21:04:21 +0100

    a

--- Source/Shifts.Web/app/app.module.ts
+++ Source/Shifts.Web/app/app.module.ts
@@ -5,12 +5,12 @@ import { AppComponent }  from './app.component';
 import { EventsListComponent }  from './events-list.component';
 import { EventThumbnailComponent} from './event-thumbnail.component'
 import { EventService } from './event.service'
-
+import { ToastrService} from './toastr.service'
 
 @NgModule({
   imports:      [ BrowserModule ],
   declarations: [ AppComponent, EventsListComponent,EventThumbnailComponent ],
-  providers:    [ EventService ],
+  providers:    [ EventService, ToastrService  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- Source/Shifts.Web/app/events-list.component.ts
+++ Source/Shifts.Web/app/events-list.component.ts
@@ -1,5 +1,6 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import { EventService} from './event.service'
+import { ToastrService} from './toastr.service'
 
 @Component({
 	moduleId: module.id,
@@ -9,13 +10,18 @@ import { EventService} from './event.service'
 				<h1>Upcoming Angular 2 Events</h1>
 				<hr>
 				<div *ngFor="let event of events">
-					<event-thumbnail [event]="event"></event-thumbnail>
+					<event-thumbnail (click)="handleClick(event.name)" [event]="event"></event-thumbnail>
 				</div>
 			</div>
 	`
 })
 export class EventsListComponent implements  OnInit {
-	constructor(private eventService: EventService) {
+	constructor(private eventService: EventService, private toastrService: ToastrService) {
+	}
+
+	handleClick(data) {
+		this.toastrService.success(data);
+		console.log(data);
 	}
 
 	ngOnInit() {

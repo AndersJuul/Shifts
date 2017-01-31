e33d72a58652fa5b5b5ba8f81a083207043b39cc
Anders Juul <anders_privat@hotmail.com>
2017-01-31 22:08:17 +0100

    a

--- Source/Shifts.Web/app/app.component.ts
+++ Source/Shifts.Web/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>
-  <events-list></events-list>`,
+  template: `
+    <nav-bar></nav-bar>
+	<events-list></events-list>
+	`,
 })
 export class AppComponent  { name = 'Angular ' + new Date().toLocaleTimeString(); }
---
+++ Source/Shifts.Web/app/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'nav-bar',
+	templateUrl: "app/navbar.component.html",
+	styles: [`
+	.pad-left {margin-left: 30px;}
+	.well  {color: #bbb;}
+	`]
+})
+export class NavBarComponent {
+}

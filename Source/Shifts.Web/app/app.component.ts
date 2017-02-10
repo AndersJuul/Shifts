31fb05f394ab41fc092095b898a1ccff2440f3af
Anders Juul <aju@vertica.dk>
2017-02-10 14:20:14 +0100

    a

--- Source/Shifts.Web/app/app.component.ts
+++ Source/Shifts.Web/app/app.component.ts
@@ -4,7 +4,9 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
     <nav-bar></nav-bar>
-	<router-outlet></router-outlet>
+    <div class="row">
+	    <router-outlet></router-outlet>
+    </div>
 	`,
 })
 export class AppComponent  { name = 'Angular ' + new Date().toLocaleTimeString(); }

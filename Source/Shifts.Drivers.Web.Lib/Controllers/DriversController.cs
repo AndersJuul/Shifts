ec22de1124d3d84c707c59ad80dd5a5db0b2c535
Anders Juul <aju@vertica.dk>
2017-02-10 13:34:50 +0100

    A

--- Source/Shifts.Drivers.Web.Lib/Controllers/DriversController.cs
+++ Source/Shifts.Drivers.Web.Lib/Controllers/DriversController.cs
@@ -1,9 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
 using System.Web.Http;
-using Dapper;
 using Shifts.Drivers.Contracts;
 
 namespace Shifts.Drivers.Web.Lib.Controllers
@@ -17,7 +15,9 @@ namespace Shifts.Drivers.Web.Lib.Controllers
             var connectionString = ConfigurationManager.ConnectionStrings["shiftDriversDb"].ConnectionString;
             using (IDbConnection db = new SqlConnection(connectionString))
             {
-                return Ok(db.Query<Driver>("Select * From Drivers").ToList());
+                //var content = db.Query<Driver>("Select * From Drivers").ToList();
+                var content = new[] {new Driver {id = 1, name = "Anders"}, new Driver {id = 2, name = "Per"}};
+                return Ok(content);
             }
         }
     }
